76d2a158c7e778baf026682961a464d5c04595b9
piyushgkb <piyushgkb@gmail.com>
2018-08-30 23:15:32 +0200

    added code

--- EWS_Code.R
+++ EWS_Code.R
@@ -28,6 +28,17 @@ env<-function(t,C){
 
 
 # function for simulating the dynamics of the model
+#dynamic() <- function that simulates the stochastic model
+#tmax<- total time
+#ind<- no. of individuals
+#omega<- width of fitness function
+#var.size<- variation in the phenotype
+#rho <- environmental predictability p
+#var.U<- variability in cue
+#var.theta <- variability in optimum phenotype
+#C<- parameter that controls how fast the Environment E shifts
+#r0 <- average. reproductive rate.
+#mean.breed<- mean breeding value
 
 dynamics<-function(tmax,ind,omega,var.size,rho,var.U, var.Theta,r0,mean.breed,beta){
   breed<-matrix(NA,nrow=ind,ncol=tmax)
